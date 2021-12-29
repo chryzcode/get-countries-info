7c4925dd794415f4f7e744ea06bcc902fb439ba8
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-12-30 00:54:18 +0100

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Country-info
+
+Getting country's information including country's map and QR-code encoding country's information! 
+- [Video on how the code runs!](https://www.youtube.com/watch?v=vCzvzrZSzeg&t=9s)
+- [Blog explaining more in detail](https://chryzcodez.hashnode.dev/how-i-made-a-country-info-project-using-python)
---
+++ main.py
@@ -0,0 +1,76 @@
+import qrcode
+import folium
+from countryinfo import CountryInfo
+
+print('Welcome, You can get information about any country here!')
+user = input('\nEnter the name of a country:\n')
+name = user
+
+country = CountryInfo(name)
+
+country_name = country.alt_spellings()
+print('\nName:')
+for name in country_name:
+    print(name, end= ', ')
+
+country_capital =country.capital()
+print(f'\n\nCapital:\n{country_capital}')
+
+country_currency = country.currencies()
+print('\nCurrency:')
+for currency in country_currency:
+    print(currency, end= ', ')
+
+country_lang = country.languages()
+print('\n\nLanguages:')
+for languages in country_lang:
+    print(languages, end =', ')
+
+country_timezone = country.timezones()
+print('\n\nTime-Zone:')
+for timezones in country_timezone:
+    print(timezones, end = ', ')
+
+country_area = country.area()
+print(f'\n\nCountry Area:\n{country_area}')
+
+country_borders = country.borders()
+print('\nBorders:')
+for border in country_borders:
+    print(border, end=', ')
+
+calling_codes= country.calling_codes()
+print('\n\nCall Code:')
+for call_code in calling_codes:
+    print(call_code)
+
+country_region = country.region()
+print(f'\nRegion:\n{country_region}')
+
+sub_region = country.subregion()
+print(f'\nSub-Region:\n{sub_region}')
+
+country_population = country.population()
+print(f'\nPopulation:\n{country_population}')
+
+country_states = country.provinces()
+print('\nStates/Provinces:')
+for states in country_states:
+    print(states, end=', ')
+
+about_country = country.wiki()
+
+country_map = country.capital_latlng()
+map = folium.Map(location = (country_map))
+print(f'\n\nCheck your directory for the map of  {user}, named: (mylocation.html)')
+map.save("mylocation.html")
+
+about_country = country.wiki()
+
+data = about_country
+qr = qrcode.QRCode( version = 1, box_size = 15, border = 5)
+qr.add_data(data)
+qr.make(fit = True)
+img = qr.make_image(fill_color = 'black', back_color = 'white')
+print(f'\nCheck your directory for the qr code containing the information of {user}, named: (about_country.png)')
+img.save('C:/Users/chryz/Desktop/about_country.png')
